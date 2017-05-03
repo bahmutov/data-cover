55f1179c38a3aff8426f22eb48acba687e8a8bdb
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-05-03 09:55:28 -0400

    printing collected data

--- src/bdd.js
+++ src/bdd.js
@@ -2,12 +2,13 @@
 
 const hook = require('node-hook')
 const debug = require('debug')('data-cover')
+const instrument = require('./instrument-source')
 
 const isSpec = filename => /spec\.js$/.test(filename)
 
 const is3rdParty = filename => /node_modules/.test(filename)
 
-function instrument(source, filename) {
+function onFileLoad(source, filename) {
   if (isSpec(filename)) {
     debug('Skipping file %s', filename)
     return source
@@ -18,11 +19,13 @@ function instrument(source, filename) {
   }
 
   debug('instrumenting %s for data-cover', filename)
-  return source
+  return instrument(source, filename)
 }
-hook.hook('.js', instrument)
+hook.hook('.js', onFileLoad)
 
 process.on('exit', function () {
   debug('data-cover is done')
   hook.unhook('.js')
+  const results = JSON.stringify(global.__instrumenter.functions, null, 2)
+  debug(results)
 })
