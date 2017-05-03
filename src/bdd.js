cd6c864b3bb9a9b9a15002e89eba991c2bd64a8f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-05-03 09:52:24 -0400

    hook and unhook

--- src/bdd.js
+++ src/bdd.js
@@ -1,3 +1,28 @@
+'use strict'
+
+const hook = require('node-hook')
+const debug = require('debug')('data-cover')
+
+const isSpec = filename => /spec\.js$/.test(filename)
+
+const is3rdParty = filename => /node_modules/.test(filename)
+
+function instrument(source, filename) {
+  if (isSpec(filename)) {
+    debug('Skipping file %s', filename)
+    return source
+  }
+  if (is3rdParty(filename)) {
+    debug('skipping node_modules file', filename)
+    return source
+  }
+
+  debug('instrumenting %s for data-cover', filename)
+  return source
+}
+hook.hook('.js', instrument)
+
 process.on('exit', function () {
-  console.log('data-cover is done')
+  debug('data-cover is done')
+  hook.unhook('.js')
 })
